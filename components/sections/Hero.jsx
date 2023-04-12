c41de2069720793a581568d169e6dbaa3e933993
Honorable Con <honorablecon@gmail.com>
2023-04-12 16:14:27 +0100

    add visualize component

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="text-black max-w-[65%] grid gap-6 justify-center items-center py-8">
+    <div className="text-black max-w-[75%] grid gap-6 justify-center items-center py-8">
       <h1 className="text-4xl font-bold text-center">
         Automatisez votre cycle de facturation
       </h1>
-      <p className="text-center leading-8">
+      <p className="text-centerf leading-8">
         {`La saisie manuelle des factures pour la transformer de son format papier en information numérique est un long processus qui est souvent source d’erreur ou omissions. Grace a notre outil, automatisez en clin d’oeil la conversation de vos factures numérisées en données utilisables par votre solution comptable ou système d’information. Profitez de nos capacités d’archivage en ligne pour trouver vos factures en un temps record a toute heure de la journée.`}
       </p>
       {/* <div className="my-3"></div> */}
--- components/sections/Upload.jsx
+++ components/sections/Upload.jsx
@@ -39,7 +39,13 @@ const Upload = () => {
   };
 
   if (loader) return <Loader />;
-  if (data) return <Download data={data} />;
+  if (data)
+    return (
+      <div className="flex space-x-8 items-start">
+        <Download data={data} />
+        <Visualize />
+      </div>
+    );
 
   return (
     <div>
@@ -74,3 +80,22 @@ const Upload = () => {
 };
 
 export default Upload;
+
+const Visualize = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <img className="mx-auto h-20" src={"/step3.svg"} alt="step indication" />
+      <h2 className="w-fit p-8 py-6 mx-auto mb-4 text-2xl bg-[#F2F2F2] rounded-[24px] shadow-[0px_4px_4px_rgba(0,0,0,0.25)]">
+        Visualisez !
+      </h2>
+      <p className="w-[85%]">
+        {"Nous avons archivé et classé votre facture numériquement*."} <br />
+        {"L’archivage numérique est légalisé au Sénégal par la loi XXX."} <br />
+        <br />
+        {
+          "Tout cela est complètement personnalisable en fonction de vos besoins en mode PRO."
+        }
+      </p>
+    </div>
+  );
+};
